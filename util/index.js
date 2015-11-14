0d04faee89aad88a5041aaf504cfc75fafea8911
Daniel Imms <tyriar@tyriar.com>
2015-11-14 01:31:56 -0800

    Fix symlinkOrReplaceSync on linux

--- util/index.js
+++ util/index.js
@@ -1,8 +1,9 @@
 var fs = require('fs');
+var process = require('process');
 var symlinkOrCopySync = require('symlink-or-copy').sync;
 
 function symlinkOrReplaceSync(srcPath, destPath) {
-  if (fs.statSync(destPath)) {
+  if (fs.existsSync(destPath)) {
     fs.unlinkSync(destPath);
   }
   symlinkOrCopySync(srcPath, destPath);
