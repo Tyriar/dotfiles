3a63b60a8c4a7ca1ca24ebbf5a6acc6ff997787f
Daniel Imms <tyriar@tyriar.com>
2015-12-08 21:13:38 -0800

    Remove legacy sublime config

--- sync-helpers/sublime-preferences.sh
+++
@@ -1,6 +0,0 @@
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-echo -e "\e[33m~~~Syncing Sublime Text 3 preferences~~~\e[0m"
-
-SUBLIME_PATH=~/.config/sublime-text-3/Packages/User
-cp $DIR/../sublime/User/* $SUBLIME_PATH
