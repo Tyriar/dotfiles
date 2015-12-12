09d3bb521ee2edc06c478644a5ba4cdd976d22b8
Daniel Imms <tyriar@tyriar.com>
2015-12-12 10:07:15 -0800

    [gnome-terminal] Install profile correctly for gnome < 3.8 and >= 3.8
    Fixes #24

--- gnome-terminal/index.js
+++ gnome-terminal/index.js
@@ -1,14 +1,28 @@
+var chalk = require('chalk');
 var execAndReportSync = require('../util/exec-and-report-sync');
 var fs = require('fs');
 var getHomePath = require('home-path');
+var gnomeTerminalVersion = require('gnome-terminal-version');
 var logHelper = require('../util/log-helper');
 var path = require('path');
 var process = require('process');
+var semver = require('semver');
 var symlinkOrReplaceFilesInFolderSync = require('../util/symlink-or-replace-files-in-folder-sync');
 
 module.exports.install = function () {
-  if (process.platform !== 'win32') {
-    logHelper.logStepStarted('gnome-terminal');
-    execAndReportSync('applying profile theme', path.join(__dirname, 'profile-preferences.sh'));
+  if (process.platform === 'linux') {
+    gnomeTerminalVersion(function (version) {
+      logHelper.logStepStarted('gnome-terminal');
+      if (version === undefined || !semver.valid(version)) {
+        logHelper.logSubStepFail('gnome-terminal doesn\'t seem to be installed');
+        return;
+      }
+
+      if (semver.major(version) == 3 && semver.minor(version) >= 8 || semver.major(version) > 3) {
+        execAndReportSync('applying profile theme (Gnome 3.8+)', path.join(__dirname, 'profile-theme-3.8.sh'));
+      } else {
+        execAndReportSync('applying profile theme', path.join(__dirname, 'profile-theme.sh'));
+      }
+    });
   }
 };
---
+++ gnome-terminal/profile-theme.sh
@@ -0,0 +1,21 @@
+# Use gconftool-2 for Gnome < 3.8
+gconftool-2 --set "/apps/gnome-terminal/profiles/Default/bold_color_same_as_fg" \
+    --type bool "true"
+gconftool-2 --set "/apps/gnome-terminal/profiles/Default/use_theme_colors" \
+    --type bool "false"
+gconftool-2 --set "/apps/gnome-terminal/profiles/Default/background_darkness" \
+    --type float "1"
+gconftool-2 --set "/apps/gnome-terminal/profiles/Default/background_type" \
+    --type string "transparent"
+gconftool-2 --set "/apps/gnome-terminal/profiles/Default/palette" \
+    --type string "#096209620962:#E6E61A1A4141:#1717E3E38584:#FFFFDEDE6A6A:#4F4F8584FFFF:#A0A07474C4C4:#4F4FA5A5C7C7:#D3D3D7D7CFCF:#404040404040:#CFCF18173A39:#0D0DC7C77272:#F4F4CDCD4545:#26265D5DD9D9:#88884E4EBABA:#2F2F62627777:#EEEEEEEEECEC"
+gconftool-2 --set "/apps/gnome-terminal/profiles/Default/background_color" \
+    --type string "#151517161818"
+gconftool-2 --set "/apps/gnome-terminal/profiles/Default/visible_name" \
+    --type string "Default"
+gconftool-2 --set "/apps/gnome-terminal/profiles/Default/bold_color" \
+    --type string "#000000000000"
+gconftool-2 --set "/apps/gnome-terminal/profiles/Default/foreground_color" \
+    --type string "#FEFEFEFEFEFE"
+gconftool-2 --set "/apps/gnome-terminal/profiles/Default/font" \
+    --type string "Hack 11"
