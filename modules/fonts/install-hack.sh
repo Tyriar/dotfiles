e7e7950a3f80e12436da91ffddca347a6cc1b5b0
Daniel Imms <tyriar@tyriar.com>
2016-01-14 12:18:48 -0800

    [fonts] Install on Linux when ~/.local/share/fonts does not exist
    Fixes #44

--- modules/fonts/install-hack.sh
+++ modules/fonts/install-hack.sh
@@ -5,10 +5,14 @@ HACK_VERSION='v2.018/Hack-v2_018-ttf.zip'
 HACK_URL=${HACK_RELEASES}${HACK_VERSION}
 TEMP=`mktemp`
 curl -L -o "${TEMP}" "${HACK_URL}"
+# OSX
 if [ -d ~/Library/Fonts ]; then
   unzip -o "${TEMP}" -d ~/Library/Fonts/
-fi
-if [ -d ~/.local/share/fonts ]; then
-  unzip -o "${TEMP}" -d ~/.local/share/fonts/
+else
+  # Linux
+  if [ -d /usr/share/fonts ]; then
+    mkdir -p ~/.fonts/typetype/hack
+    unzip -o "${TEMP}" -d ~/.fonts/typetype/hack
+  fi
 fi
 rm "${TEMP}"
