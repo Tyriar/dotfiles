b57bc8dfaf6bf741b29cb1747b052df921b4dd60
Daniel Imms <tyriar@tyriar.com>
2016-04-10 01:28:07 -0700

    Allow bootstrap to be called from any dir

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,36 +1,44 @@
 if [ -f /etc/debian_version ]; then
+  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  
   sudo apt-get update
   sudo apt-get install -y curl git vim
   
   # Install Chrome
   sudo apt-get install -y libappindicator1
   sudo apt-get install -f
-  mkdir temp && cd temp
+  pushd $(mktemp -d)
   wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.  deb
   sudo dpkg -i google-chrome-stable_current_amd64.deb
-  cd .. && rm -rf temp
+  popd
   
   # Install Visual Studio Code Stable
-  mkdir temp && cd temp
+  pushd $(mktemp -d)
   wget https://vscode-update.azurewebsites.net/api/deb/stable/amd64/deb/vscod  e-amd64.deb
   sudo dpkg -i vscode-amd64.deb
-  cd .. && rm -rf temp
+  popd
   
   # Install Visual Studio Code Insiders
-  mkdir temp && cd temp
+  pushd $(mktemp -d)
   wget https://vscode-update.azurewebsites.net/api/deb/insider/amd64/deb/vsco  de-amd64.deb
   sudo dpkg -i vscode-amd64.deb
-  cd .. && rm -rf temp
+  popd
+  
+  # Install Atom
+  pushd $(mktemp -d)
+  wget -O atom-amd64.deb https://atom.io/download/deb
+  sudo dpkg -i atom-amd64.deb
+  popd
   
   # Configure Unity Launcher
   gsettings set com.canonical.Unity.Launcher favorites "['application://ubiquity.desktop',   'application://google-chrome.desktop',   'application://gnome-terminal.desktop',   'application://nautilus.desktop',   'application://code-insiders.desktop']"
   
   # Install node stable
-  ./install_deps.sh
+  $DIR/install_deps.sh
   
   # Install modules
   npm install
-  node cli.js install
+  node $DIR/../cli.js install
 else
   echo "Only Debian is supported"
 fi
