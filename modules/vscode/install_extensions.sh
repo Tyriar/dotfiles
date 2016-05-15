14d08a4da7e4ed89458bdb553dc4789173464def
Daniel Imms <tyriar@tyriar.com>
2016-05-15 09:11:30 -0700

    Improve install extensions to support multiple variants
    Fixes #60

--- modules/vscode/install_extensions.sh
+++ modules/vscode/install_extensions.sh
@@ -1,11 +1,18 @@
-for EXTENSION in "EditorConfig.EditorConfig" \
-                 "Tyriar.sort-lines" \
-                 "Tyriar.lorem-ipsum" \
-                 "cssho.vscode-svgviewer" \
-                 "dbaeumer.vscode-eslint" \
-                 "ms-vscode.csharp" \
-                 "ryzngard.vscode-header-source" \
-                 "waderyan.gitblame"
+# Install extensions for variants supporting --install-extension
+for VARIANT in "code-alpha"
 do
-    code-alpha --install-extension $EXTENSION
+  if hash $VARIANT 2>/dev/null; then
+    echo "Installing extensions for $VARIANT"
+    for EXTENSION in "EditorConfig.EditorConfig" \
+                    "Tyriar.sort-lines" \
+                    "Tyriar.lorem-ipsum" \
+                    "cssho.vscode-svgviewer" \
+                    "dbaeumer.vscode-eslint" \
+                    "ms-vscode.csharp" \
+                    "ryzngard.vscode-header-source" \
+                    "waderyan.gitblame"
+    do
+      $VARIANT --install-extension $EXTENSION
+    done
+  fi
 done
