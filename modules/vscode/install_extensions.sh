c82410c24b2d0ff2f7fc1b562e7109ec143cd42f
Daniel Imms <tyriar@tyriar.com>
2016-06-04 17:36:53 -0700

    [vscode] Improve install extension script

--- modules/vscode/install_extensions.sh
+++ modules/vscode/install_extensions.sh
@@ -1,19 +1,31 @@
 # Install extensions for variants supporting --install-extension
-for VARIANT in "code-alpha"
+EXTENSIONS=(
+  "cssho.vscode-svgviewer" \
+  "dbaeumer.vscode-eslint" \
+  "EditorConfig.EditorConfig" \
+  "ryzngard.vscode-header-source" \
+  "spywhere.guides" \
+  "Tyriar.sort-lines" \
+  "Tyriar.lorem-ipsum" \
+  "waderyan.gitblame"
+)
+
+for VARIANT in "code" \
+               "code-insiders" \
+               "code-alpha"
 do
   if hash $VARIANT 2>/dev/null; then
-    echo "Installing extensions for $VARIANT"
-    for EXTENSION in "cssho.vscode-svgviewer" \
-                    "dbaeumer.vscode-eslint" \
-                    "EditorConfig.EditorConfig" \
-                    "ms-vscode.csharp" \
-                    "ryzngard.vscode-header-source" \
-                    "spywhere.guides" \
-                    "Tyriar.sort-lines" \
-                    "Tyriar.lorem-ipsum" \
-                    "waderyan.gitblame"
-    do
-      $VARIANT --install-extension $EXTENSION
-    done
+    VERSION="$($VARIANT --version)"
+    MAJOR="$(echo $VERSION | sed -e 's/\..*$//')"
+    MINOR="$(echo $VERSION | sed -e 's/\-[a-z]*$//' -e 's/^[0-9]*\.//' -e 's/\.[0-9]*$//')"
+    if [ "$MAJOR" -gt "0" ] && [ "$MINOR" -gt "1" ]; then
+      echo "Installing extensions for $VARIANT"
+      for EXTENSION in ${EXTENSIONS[@]}
+      do
+        $VARIANT --install-extension $EXTENSION
+      done
+    else
+      echo "$VARIANT (v$VERSION) does not support extension CLI args"
+    fi
   fi
 done
