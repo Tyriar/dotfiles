84f8711ca603a61bb9c3ebf6f0d56dcf24a4b398
Daniel Imms <tyriar@tyriar.com>
2016-09-10 18:48:46 -0700

    [vscode] Remove version checker for ext install support

--- modules/vscode/install_extensions.sh
+++ modules/vscode/install_extensions.sh
@@ -15,17 +15,10 @@ for VARIANT in "code" \
                "code-insiders"
 do
   if hash $VARIANT 2>/dev/null; then
-    VERSION="$($VARIANT --version)"
-    MAJOR="$(echo $VERSION | sed -e 's/\..*$//')"
-    MINOR="$(echo $VERSION | sed -e 's/\-[a-z]*$//' -e 's/^[0-9]*\.//' -e 's/\.[0-9]*$//')"
-    if [ "$MAJOR" -gt "0" ] && [ "$MINOR" -gt "1" ]; then
-      echo "Installing extensions for $VARIANT"
-      for EXTENSION in ${EXTENSIONS[@]}
-      do
-        $VARIANT --install-extension $EXTENSION
-      done
-    else
-      echo "$VARIANT (v$VERSION) does not support extension CLI args"
-    fi
+    echo "Installing extensions for $VARIANT"
+    for EXTENSION in ${EXTENSIONS[@]}
+    do
+      $VARIANT --install-extension $EXTENSION
+    done
   fi
 done
