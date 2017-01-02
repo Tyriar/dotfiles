cf62992c827090453b1b8c71409f5f3981717dd8
Daniel Imms <tyriar@tyriar.com>
2017-01-01 16:22:36 -0800

    [git] Install .gitconfig on windows

--- modules/git/index.js
+++ modules/git/index.js
@@ -6,13 +6,11 @@ var process = require('process');
 var symlinkOrReplaceFilesInFolderSync = require('../../util/symlink-or-replace-files-in-folder-sync');
 
 module.exports.install = function () {
-  if (process.platform !== 'win32') {
-    logHelper.logStepStarted('git');
-    var sourceDir = path.join(__dirname, 'config');
-    var destDir = path.join(getHomePath());
-    var files = fs.readdirSync(sourceDir);
-    logHelper.logSubStepPartialStarted('applying config files');
-    symlinkOrReplaceFilesInFolderSync(files, sourceDir, destDir);
-    logHelper.logSubStepPartialSuccess();
-  }
+  logHelper.logStepStarted('git');
+  var sourceDir = path.join(__dirname, 'config');
+  var destDir = path.join(getHomePath());
+  var files = fs.readdirSync(sourceDir);
+  logHelper.logSubStepPartialStarted('applying config files');
+  symlinkOrReplaceFilesInFolderSync(files, sourceDir, destDir);
+  logHelper.logSubStepPartialSuccess();
 };
