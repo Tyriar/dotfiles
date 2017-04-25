718ac2420c095e1cd84521d1bd379e6a4cd024be
Daniel Imms <tyriar@tyriar.com>
2017-04-25 10:11:11 -0700

    Remove arc theme

--- scripts/setup_ubuntu.sh
+++ scripts/setup_ubuntu.sh
@@ -34,19 +34,6 @@ if [ -f /etc/debian_version ]; then
   sudo apt update
   sudo apt install code code-insiders
 
-  # Install arc theme and icon theme
-  echo -e '\e[34;47mInstalling arc theme and icon theme\e[0m'
-  sudo apt update
-  sudo apt install -y arc-theme
-  sudo apt install -y unity-tweak-tool
-  wget -O $DIR/temp/arc-icon-theme.tar.gz "https://github.com/horst3180/arc-icon-theme/archive/20161122.tar.gz"
-  mkdir -p $DIR/temp/arc-icon-theme
-  tar -xf $DIR/temp/arc-icon-theme.tar.gz -C $DIR/temp/arc-icon-theme
-  mkdir -p ~/.icons
-  mv $DIR/temp/arc-icon-theme/arc-icon-theme-20161122/Arc ~/.icons/Arc
-  echo 'Set theme and icon theme'
-  unity-tweak-tool
-
   # Configure Ubuntu
   echo -e '\e[34;47mConfiguring Ubuntu\e[0m'
   gsettings set com.canonical.Unity.Launcher favorites "['application://ubiquity.desktop', 'application://google-chrome.desktop', 'application://gnome-terminal.desktop', 'application://org.gnome.Nautilus.desktop', 'application://code-insiders.desktop', 'application://slack.desktop', 'application://update-manager.desktop']"
