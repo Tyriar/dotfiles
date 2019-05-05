bbc53535cede7da4d08db14ab62d4193800adde3
Daniel Imms <tyriar@tyriar.com>
2019-05-04 23:23:47 -0700

    Remove sync-exec

--- src/util/exec.ts
+++ src/util/exec.ts
@@ -1,15 +1,16 @@
 import * as logHelper from '../util/log-helper';
 import * as os from 'os';
-const syncExec = require('sync-exec');
+import { execSync } from 'child_process';
 
 export function execAndReportSync(actionName: string, command: string): void {
   logHelper.logSubStepPartialStarted(actionName);
-  const result = syncExec(command);
-  if (result.status === 0) {
+
+  try {
+    execSync(command);
     const successChar = process.platform === 'win32' ? '\u221A' : '✔';
     process.stdout.write(` \x1b[32m${successChar}\x1b[0m${os.EOL}`);
-  } else {
+  } catch (err) {
     logHelper.logSubStepFail('Error running command: ' + command);
-    console.error(result);
+    console.error(err);
   }
 }
