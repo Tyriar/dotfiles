0b60c0ec54306205c5fc801a37a01d65cbba8d51
Daniel Imms <tyriar@tyriar.com>
2019-05-24 08:55:17 -0700

    Set npm prefix after node is added to path

--- scripts/setup_deb.sh
+++ scripts/setup_deb.sh
@@ -36,8 +36,8 @@ sudo apt-get update && sudo apt-get install -y yarn
 step "Setup .bashrc_local"
 rm -rf ~/.bashrc_local
 cat <<EOT > ~/.bashrc_local
-npm set prefix ~/.npm-packages
 export PATH=~/.npm-packages/bin:~/tools/$NODE_NAME/bin:/snap/bin:\$PATH
+npm set prefix ~/.npm-packages
 alias git=hub
 alias c=code-insiders
 EOT
