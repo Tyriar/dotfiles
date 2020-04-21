1f500db3b951ca9525ff23759cf07b987ddeaf07
Daniel Imms <tyriar@tyriar.com>
2020-04-21 09:34:48 -0700

    Update pwsh profile

--- data/pwsh/Profile.ps1
+++ data/pwsh/Profile.ps1
@@ -1,1 +1,8 @@
+# Make tab act like bash
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
+
+# Disable tab completion bell
+Set-PSReadlineOption -BellStyle None
+
+# Aliases
 Set-Alias c code-insiders.cmd
