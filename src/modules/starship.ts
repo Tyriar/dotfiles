f716ecb01db1096f2a73ed2450b154a7405c1a37
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2021-06-17 08:18:00 -0700

    xplat starship

--- src/modules/starship.ts
+++ src/modules/starship.ts
@@ -4,13 +4,11 @@ import * as logHelper from '../util/log-helper';
 import * as path from 'path';
 
 export function install(): void {
-  if (process.platform === 'win32') {
-    logHelper.logStepStarted('starship');
-    const sourceDir = path.join(__dirname, '../../data/starship');
-    const destDir = path.join(process.env.USERPROFILE || process.env.HOME!, '.config');
-    const files = fs.readdirSync(sourceDir);
-    logHelper.logSubStepPartialStarted('applying config files');
-    symlinkOrReplaceFilesInFolderSync(files, sourceDir, destDir);
-    logHelper.logSubStepPartialSuccess();
-  }
+  logHelper.logStepStarted('starship');
+  const sourceDir = path.join(__dirname, '../../data/starship');
+  const destDir = path.join(process.env.USERPROFILE || process.env.HOME!, '.config');
+  const files = fs.readdirSync(sourceDir);
+  logHelper.logSubStepPartialStarted('applying config files');
+  symlinkOrReplaceFilesInFolderSync(files, sourceDir, destDir);
+  logHelper.logSubStepPartialSuccess();
 }
